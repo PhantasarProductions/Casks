e467795ac9a29fea575ef26647659acf0bf832f9
Tricky1975 <facebook@tbbs.nl>
2017-04-05 23:07:41 +0200

    closed #2 -- Scyndi's Forest Quest added

---
+++ casks/scyndi.rb
@@ -0,0 +1,11 @@
+cask 'scyndi' do
+  version 'beta release'
+
+  url "https://downloads.sourceforge.net/sfq/https://sourceforge.net/projects/sfq/files/Beta%20Release/SFQ_BETA_MacOS_X_Intel.zip/download"
+  
+  name "Scyndi's Forest Quest"
+  homepage 'http://gamejolt.com/games/scyndi-s-forest-quest/44966'
+
+  app "Scyndi's Forest Quest.app"
+end
+
