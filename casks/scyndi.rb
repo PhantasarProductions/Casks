b082292c03743c27938bf89eae20ecb0b5bbc323
Tricky1975 <facebook@tbbs.nl>
2017-04-05 23:16:58 +0200

    #2 It really 'needs' a sha?

--- casks/scyndi.rb
+++ casks/scyndi.rb
@@ -2,6 +2,7 @@ cask 'scyndi' do
   version 'beta release'
 
   url "https://downloads.sourceforge.net/sfq/Beta%20Release/SFQ_BETA_MacOS_X_Intel.zip"
+  sha256 'ea86686df59167721bf316979f343be39a6a26393df1fe2f07c33906e874113d'
   
   name "Scyndi's Forest Quest"
   homepage 'http://gamejolt.com/games/scyndi-s-forest-quest/44966'
