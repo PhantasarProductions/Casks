b19bd02715fd36d6031007aea33ce3e3684d5b73
Tricky1975 <facebook@tbbs.nl>
2017-04-05 15:08:56 +0200

    Test number how much?

---
+++ hbcasks/dyrtrpg.rb
@@ -0,0 +1,12 @@
+cask 'dyrt' do
+  version '17.04.05'
+  sha256 '8ba62dde31c26f9b7cdb6a73d3deb83c272665fbd0630eee339d8e3e22d2bcf1'
+
+  url "https://downloads.sourceforge.net/dyrt/Game/17.04.05%20%28stable%29/The%20Secrets%20of%20Dyrt%20-%20Mac.zip"
+  
+  name 'The Secrets of Dyrt'
+  homepage 'http://dyrt.sourceforge.net/'
+
+  app "The Secrets of Dyrt.app"
+end
+
