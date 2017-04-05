e6381a5b92b3f0408635e4e15dda882581a4c950
Tricky1975 <facebook@tbbs.nl>
2017-04-05 14:41:48 +0200

    Is #1 fixed or is it not?

--- Casks/Dyrt.rb
+++ Casks/Dyrt.rb
@@ -2,7 +2,9 @@ cask 'dyrt' do
   version '17.04.05'
   sha256 '8ba62dde31c26f9b7cdb6a73d3deb83c272665fbd0630eee339d8e3e22d2bcf1'
 
-  url "https://sourceforge.net/projects/dyrt/files/Game/17.04.05%20%28stable%29/The%20Secrets%20of%20Dyrt%20-%20Mac.zip/download.zip"
+  # url "https://sourceforge.net/projects/dyrt/files/Game/17.04.05%20%28stable%29/The%20Secrets%20of%20Dyrt%20-%20Mac.zip/download.zip"
+  url "https://downloads.sourceforge.net/dyrt/Game/17.04.05%20%28stable%29/The%20Secrets%20of%20Dyrt%20-%20Mac.zip"
+  
   name 'The Secrets of Dyrt'
   homepage 'http://dyrt.sourceforge.net/'
 
