b3381d68e90d9f33ea6aae7196df1f77ff9a950b
Tricky1975 <facebook@tbbs.nl>
2017-04-06 08:29:04 +0200

    Fixed an issue in #3
    	modified:   casks/starstory.rb

--- casks/starstory.rb
+++ casks/starstory.rb
@@ -1,7 +1,6 @@
 cask 'starstory' do
   version '16.11.26'
 
-  url "https://downloads.sourceforge.net/sfq/Beta%20Release/SFQ_BETA_MacOS_X_Intel.zip"
   url "https://downloads.sourceforge.net/starstory/Stable%20Releases/Mac/Zipped/StarStoryMac_Music.zip"
   sha256 'StarStoryBetaMac_Music.zip'
   
