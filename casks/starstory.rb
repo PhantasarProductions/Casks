85498132f88f68808e8b8a573025e96c2e3d77e9
Tricky1975 <facebook@tbbs.nl>
2017-04-14 17:28:00 +0200

    Fixed url Star Story

--- casks/starstory.rb
+++ casks/starstory.rb
@@ -1,7 +1,7 @@
 cask 'starstory' do
   version '17.04.14'
 
-  url "https://downloads.sourceforge.net/starstory/Stable%20Releases/#{version}/Mac/Zipped/StarStoryMac_Music.zip"
+  url "https://downloads.sourceforge.net/starstory/Stable%20Releases/#{version}/Mac/Zipped/StarStory_Mac_Music.zip"
   sha256 '1be7bdf86ed63acd91881c1fb2f86280653dcfd1b00436f687a54942d309b4fc'
   
   name "Star Story"
