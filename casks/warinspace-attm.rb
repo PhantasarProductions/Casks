5ec36bdf08271ad35d2408bb1ac58e61617144be
Tricky1975 <facebook@tbbs.nl>
2017-05-01 22:01:27 +0200

    War In Space -- All around the moon

---
+++ casks/warinspace-attm.rb
@@ -0,0 +1,11 @@
+cask 'scyndi' do
+  version 'beta release'
+
+  url "https://downloads.sourceforge.net/warinspace/%52%65%6c%65%61%73%65%20%31%32%2e%30%35%2e%30%39/WiS_12.05.09_MacOS_Universal.zip"
+  sha256 'b0f9d2aca1211a01985e370102a095c6ccf8a73d007a7139ed6b33d495d67e7a'  
+  name "War in Space - All Around The Moon"
+  homepage 'https://gamejolt.com/games/war-in-space-all-around-the-moon/19201'
+
+  app "WiS.app"
+end
+
