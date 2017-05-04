72705f76ed59a344bb8ec84c19e1fb6d4e99636d
Tricky1975 <facebook@tbbs.nl>
2017-05-05 00:56:25 +0200

    ballplay-cupid version Stable

---
+++ casks/ballplay-cupid.rb
@@ -0,0 +1,9 @@
+cask 'ballplay-cupid' do
+	version 'Stable
+	url 'https://downloads.sourceforge.net/ballplaygenius/Build/\66\97\08\08\80\08\97\21\32\71\01\10\05\17\15\32\77\97\99\79\83\32\88\32\40\20\56\54\41\46\00\09\03
+	name 'BallPlay Genius
+	homepage 'http://gamejolt.com/games/ballplay-genius/17418
+	app 'BallPlay Genius.app
+	sha256 '8cf5ce27d21490c24eedf91e0ac2bc4a748ba8f4eb20cb7c1fc9442d2d580008'
+end
+
