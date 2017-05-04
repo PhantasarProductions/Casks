1d2b2fc2274d6093322e85ecae6c1e3f5ae6050c
Tricky1975 <facebook@tbbs.nl>
2017-05-05 01:06:56 +0200

    ballplay-genius version Stable

--- casks/ballplay-genius.rb
+++ casks/ballplay-genius.rb
@@ -1,9 +1,9 @@
 cask "ballplay-genius" do
 	version "Stable"
-	url "https://downloads.sourceforge.net/ballplaygenius/build/BallPlay Genius MacOS X (x86).dmg"
+	url "https://downloads.sourceforge.net/ballplaygenius/build/\66\97\08\08\80\08\97\21\32\71\01\10\05\17\15\32\77\97\99\79\83\32\88\32\40\20\56\54\41\46\00\09\03"
 	name "BallPlay Genius"
 	homepage "http://gamejolt.com/games/ballplay-genius/17418"
 	app "BallPlay Genius.app"
-	sha256 '86208dac34a9390a2321527c25700d3c50df8a175de6bd813393b76b5d454268'
+	sha256 '8cf5ce27d21490c24eedf91e0ac2bc4a748ba8f4eb20cb7c1fc9442d2d580008'
 end
 
