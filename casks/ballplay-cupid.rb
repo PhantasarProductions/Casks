9099db16276374294714d767fde879d9a76ac281
Tricky1975 <facebook@tbbs.nl>
2017-05-05 14:55:12 +0200

    ballplay-cupid version 16.05.30

--- casks/ballplay-cupid.rb
+++ casks/ballplay-cupid.rb
@@ -3,7 +3,7 @@ cask "ballplay-cupid" do
 	url "https://downloads.sourceforge.net/ballplay-cupid/#{version}/BallPlayCupid_Stable1_v#{version}_Mac64.zip"
 	name "BallPlay Cupid"
 	homepage "http://gamejolt.com/games/bpc/136187"
-	app "BallPlay Cupid.app"
+	app "BallPlayCupid.app"
 	sha256 'eb6d9fc74d14b32428c02ce8ba5b2b6ac26602df3dfed989098c27247b3c88f4'
 end
 
