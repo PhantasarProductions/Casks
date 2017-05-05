904beb3842efb5cad96fb2960338787a546b7059
Tricky1975 <facebook@tbbs.nl>
2017-05-05 10:07:28 +0200

    potr2 version BlitzMax Port

---
+++ casks/potr2.rb
@@ -0,0 +1,9 @@
+cask "potr2" do
+	version "BlitzMax Port"
+	url "https://downloads.sourceforge.net/potr2/mac/POTR2_Mac_Universal.zip"
+	name "Power Of The Rings II"
+	homepage "https://sourceforge.net/projects/potr2/?source=navbar"
+	app "POTR2.app"
+	sha256 'fdc9cac84d625062446719766ad3567a75dc6714804cb059297b7b692e92e861'
+end
+
