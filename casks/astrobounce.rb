ca91f4166139e3c6bf033d5f7e504cbd39fc6fcd
Tricky1975 <facebook@tbbs.nl>
2017-05-05 09:24:57 +0200

    astrobounce version 13.08.13

---
+++ casks/astrobounce.rb
@@ -0,0 +1,9 @@
+cask "astrobounce" do
+	version "13.08.13"
+	url "https://downloads.sourceforge.net/astrobounce/Release%2013.08.13/AstroBounce_Mac.zip"
+	name "BallPlay Genius"
+	homepage "http://gamejolt.com/games/astrobounce/17334"
+	app "Astrobounce"
+	sha256 'a00b65f26b8bbd11b217244de4820487704de49097023f709c358c0732728d24'
+end
+
