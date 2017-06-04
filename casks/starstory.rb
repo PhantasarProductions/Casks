27e898aa2a0f2bfde8be47d166de2d761bcaf4a3
Tricky1975 <facebook@tbbs.nl>
2017-06-04 11:45:32 +0200

    version error Star Story fixed

--- casks/starstory.rb
+++ casks/starstory.rb
@@ -1,5 +1,5 @@
 cask 'starstory' do
-  version '17.064.04'
+  version '17.06.04'
 
   url "https://downloads.sourceforge.net/starstory/Stable%20Releases/#{version}/Mac/StarStory_Mac_Music.zip"
   sha256 'b785da1484e253d22d42049ea98a953ba1d1a6b3bdea03b7687c492e976e7dc5'
