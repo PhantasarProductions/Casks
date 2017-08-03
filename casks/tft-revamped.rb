183c8c5010c857814ff45832417b63c8bba29950
Tricky1975 <facebook@tbbs.nl>
2017-08-03 12:14:23 +0200

    The Fairy Tale REVAMPED
    Closes #4
    Closes phantasarproductions/tftrevamp#452

---
+++ casks/tft-revamped.rb
@@ -0,0 +1,9 @@
+cask "tft-revamped" do
+	version "17.08.02-beta"
+	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/#{version}/TFT_WN_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Windows_NoneAudio.7z"
+	name "The Fairy Tale REVAMPED"
+	homepage "https://tricky.gamejolt.io/tft"
+	app "The Fairy Tale REVAMPED.app"
+	sha256 '38568ddc1d8c5ebcbb8fb9c634073a7865e148f425fe7e0f894b0b46e3e23b4a'
+end
+
