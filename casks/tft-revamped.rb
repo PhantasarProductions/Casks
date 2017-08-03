8fe62307e6ac106dc016aa5dcfb6b97082c908f3
Tricky1975 <facebook@tbbs.nl>
2017-08-03 12:24:14 +0200

    Homebrew hates 7z???

--- casks/tft-revamped.rb
+++ casks/tft-revamped.rb
@@ -1,9 +1,9 @@
 cask "tft-revamped" do
 	version "17.08.02-beta"
-	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/#{version}/TFT_WN_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Windows_NoneAudio.7z"
+	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/#{version}/TFT_WN_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Windows_NoneAudio.tar.gz"
 	name "The Fairy Tale REVAMPED"
 	homepage "https://tricky.gamejolt.io/tft"
 	app "The Fairy Tale REVAMPED.app"
-	sha256 '09dd2b6ce0548e53ea847af94e72420e9112d2f0d7961b35ae311c48966d0c8d'
+	sha256 'b0c02760797ef757c3b177b1628ba85c2f96e8cafaa4e9448a8150d4e1004156'
 end
 
