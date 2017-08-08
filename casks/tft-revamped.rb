840ed5ec3e74a95b439374940631b6edef6d0bdc
Tricky1975 <facebook@tbbs.nl>
2017-08-08 22:20:06 +0200

    Fixed empty application field

--- casks/tft-revamped.rb
+++ casks/tft-revamped.rb
@@ -3,6 +3,6 @@ cask "tft-revamped" do
 	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/17.08.08-beta/TFT_MF_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Mac_FullAudio.zip"
 	name "The Fairy Tale REVAMPED"
 	homepage "https://tricky.gamejolt.io/tft"
-	app ""
+	app "The Fairy Tale REVAMPED.app"
 	sha256 "1ae2689ecd307d207de453e45ef924c13d45ffb0638d63310b38cbdaaa86ca2d"
 end
