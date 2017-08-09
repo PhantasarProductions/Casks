8eba626fdaec2ed17ff7f9e10c6d0710c63ed815
Tricky1975 <facebook@tbbs.nl>
2017-08-09 20:04:54 +0200

    The Fairy Tale REVAMPED v17.08.09-beta

--- casks/tft-revamped.rb
+++ casks/tft-revamped.rb
@@ -1,8 +1,8 @@
 cask "tft-revamped" do
-	version "17.08.08a-beta-cask-fix"
-	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/17.08.08-beta/TFT_MF_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Mac_FullAudio.zip"
+	version "17.08.09-beta"
+	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/17.08.09-beta/TFT_MF_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Mac_FullAudio.zip"
 	name "The Fairy Tale REVAMPED"
 	homepage "https://tricky.gamejolt.io/tft"
 	app "The Fairy Tale REVAMPED.app"
-	sha256 "1ae2689ecd307d207de453e45ef924c13d45ffb0638d63310b38cbdaaa86ca2d"
+	sha256 "92a0fd1613bde35ed29f9d6f8b22c63567336caefe5ef7b921a600c7081c395a"
 end
