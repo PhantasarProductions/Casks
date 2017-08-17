652269239ca8d5dd26a44dc0183f9e47fa4e4bdc
Tricky1975 <facebook@tbbs.nl>
2017-08-17 10:58:28 +0200

    Manual Cask corrections for The Fairy Tale REVAMPED as a few things went wrong on the last update

--- casks/tft-revamped.rb
+++ casks/tft-revamped.rb
@@ -1,6 +1,6 @@
 cask "tft-revamped" do
-	version "17.08.17-beta"
-	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/17.08.17-beta/TFT_MF_The_Fairy_Tale_REVAMPED_Pre-Beta_for_Mac_FullAudio.zip"
+	version "17.08.17a-beta-cask-correction-as-not-only-I-was-stupid-but-sourceforge-did-crazy-things-too"
+	url "https://downloads.sourceforge.net/the-fairy-tale-revamped/17.08.17-beta/17.08.17-beta/TFT_MF_The_Fairy_Tale_REVAMPED_Beta_for_Mac_FullAudio.zip"
 	name "The Fairy Tale REVAMPED"
 	homepage "https://tricky.gamejolt.io/tft"
 	app "The Fairy Tale REVAMPED.app"
