d971bc1195fa139dec32e759c0599922238ca7ab
Tricky1975 <facebook@tbbs.nl>
2018-01-23 20:42:45 +0100

    Nog meer gezwam

--- casks/starstory.rb
+++ casks/starstory.rb
@@ -3,7 +3,9 @@ cask 'starstory' do
 
   #url "https://downloads.sourceforge.net/starstory/Stable%20Releases/#{version}/Mac/StarStory_Mac_Music.zip"
   url "https://iweb.dl.sourceforge.net/project/starstory/Stable%20Releases/#{version}/Mac/StarStory_Mac_Music.zip"
-  sha256 '70dc7fc3db615630e8991932bee5db304f79b029b910de680566d083d22d9193'
+#  sha256 '70dc7fc3db615630e8991932bee5db304f79b029b910de680566d083d22d9193'
+#  sha256 '70dc7fc3db615630e8991932bee5db304f79b029b910de680566d083d22d9193'
+  sha256 'b785da1484e253d22d42049ea98a953ba1d1a6b3bdea03b7687c492e976e7dc5'
   
   name "Star Story"
   homepage 'http://starstory.sourceforge.net'
