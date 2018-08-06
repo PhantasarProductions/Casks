0226cc8081864734239cbf398a7a1f9ce53cf5f2
Tricky1975 <facebook@tbbs.nl>
2018-08-07 00:27:11 +0200

    The Secrets of Dyrt updated to v18.08.03

--- casks/dyrtrpg.rb
+++ casks/dyrtrpg.rb
@@ -1,8 +1,8 @@
 cask 'dyrtrpg' do
-  version '17.04.05'
+  version '18.08.03'
   sha256 '8ba62dde31c26f9b7cdb6a73d3deb83c272665fbd0630eee339d8e3e22d2bcf1'
 
-  url "https://downloads.sourceforge.net/dyrt/Game/17.04.05%20%28stable%29/The%20Secrets%20of%20Dyrt%20-%20Mac.zip"
+  url "https://netix.dl.sourceforge.net/project/dyrt/Game/18.08.03/Dyrt_v18.08.03_Mac_Full.zip"
   
   name 'The Secrets of Dyrt'
   homepage 'http://dyrt.sourceforge.net/'
