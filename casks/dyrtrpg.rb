be00d02383886f9abe4e9afad6756f77dc526d25
Tricky1975 <facebook@tbbs.nl>
2018-08-07 00:42:16 +0200

    checksum

--- casks/dyrtrpg.rb
+++ casks/dyrtrpg.rb
@@ -1,6 +1,6 @@
 cask 'dyrtrpg' do
   version '18.08.03'
-  sha256 '8ba62dde31c26f9b7cdb6a73d3deb83c272665fbd0630eee339d8e3e22d2bcf1'
+  sha256 'b8522f5cdd4196747375cc30276c4552cf4c899f588481344fcbab2d061427ac'
 
   url "https://netix.dl.sourceforge.net/project/dyrt/Game/18.8.3/Dyrt_v18.08.03_Mac_Full.zip"
   
