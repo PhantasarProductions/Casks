9c8ac13a6a4e30e59132c81e5ed7692cf2cfa6e1
Tricky1975 <facebook@tbbs.nl>
2018-08-07 00:37:19 +0200

    I think I fixed #8

--- casks/dyrtrpg.rb
+++ casks/dyrtrpg.rb
@@ -2,7 +2,7 @@ cask 'dyrtrpg' do
   version '18.08.03'
   sha256 '8ba62dde31c26f9b7cdb6a73d3deb83c272665fbd0630eee339d8e3e22d2bcf1'
 
-  url "https://netix.dl.sourceforge.net/project/dyrt/Game/18.08.03/Dyrt_v18.08.03_Mac_Full.zip"
+  url "https://netix.dl.sourceforge.net/project/dyrt/Game/18.8.3/Dyrt_v18.08.03_Mac_Full.zip"
   
   name 'The Secrets of Dyrt'
   homepage 'http://dyrt.sourceforge.net/'
