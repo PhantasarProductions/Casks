2272b1a60abe1849181e5d1d11bfac205006c19a
Tricky1975 <facebook@tbbs.nl>
2018-10-02 15:34:14 +0200

    MyData 18.10.2

--- casks/mydata.rb
+++ casks/mydata.rb
@@ -1,5 +1,5 @@
 cask 'mydata' do
-  version '18.9.17'
+  version '18.10.02'
 
   url "https://github.com/TrickyGameTools/MyData-Mono/files/2438028/MyData_Mac.zip"
   sha256 '2432e7debc4182625ee1690c930ce5746250bf837a8279175e63e93b3edb732f'  
