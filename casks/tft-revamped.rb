e205d67ec94fc7c803e0cd7662366d516229a88e
Tricky1975 <facebook@tbbs.nl>
2018-10-29 09:55:19 +0100

    The Fairy Tale REVAMPED v18.10.28

--- casks/tft-revamped.rb
+++ casks/tft-revamped.rb
@@ -1,10 +1,10 @@
 cask "tft-revamped" do
-	version "18.06.02"
-	url "https://netix.dl.sourceforge.net/project/the-fairy-tale-revamped/18.06.02/TFT_MF_The_Fairy_Tale_REVAMPED_Beta_for_Mac_FullAudio.zip"
+	version "18.10.28"
+	url "https://netix.dl.sourceforge.net/project/the-fairy-tale-revamped/18.10.28/TFT_MF_The_Fairy_Tale_REVAMPED_Beta_for_Mac_FullAudio.zip"
 	name "The Fairy Tale REVAMPED"
 	homepage "https://tricky.gamejolt.io/tft"
 	app "The Fairy Tale REVAMPED.app"
-	sha256 "513a1ba6c6ee2d2ed5b50631a54729be57690ea00821715c47c0813cf61f9123"
+	sha256 ""
 	caveats <<~EOS
 █████████████████████████████████████████████████████████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█████████████████████████████████████████████████████████
 ████████████████████████████████████████████████████████████▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████████████████████████████████████████████████
@@ -77,6 +77,6 @@ cask "tft-revamped" do
 Thank you for installing and trying, The Fairy Tale REVAMPED!!
 
 		Written by: Jeroen P. Broks
-		Version: 18.06.02
+		Version: 18.10.28
 	EOS
 end
